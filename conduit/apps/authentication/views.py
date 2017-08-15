c2ec46f960fb15909a52a7bd0d96692e060b5892
Aakash Sethi <aakashsethi20@gmail.com>
2017-08-14 23:57:25 -0400

    added endpoint for Login, error handling left

--- conduit/apps/authentication/views.py
+++ conduit/apps/authentication/views.py
@@ -3,7 +3,9 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegistrationSerializer
+from .serializers import (
+    RegistrationSerializer, LoginSerializer
+)
 
 from .renderers import UserJSONRenderer
 
@@ -21,4 +23,17 @@ class RegistrationAPIView(APIView):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    renderer_classes = (UserJSONRenderer,)
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        user = request.data.get('user', {})
+
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
